76f3af45745b68ced5c3c7d6038a6548b5550782
igorro79 <griniov.igor@gmail.com>
2022-11-09 23:46:29 +0200

    add pakages, set font family, add carusel slider. Index page working

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,19 +2,19 @@ import * as React from "react";
 import { Helmet } from "react-helmet";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import "./all.sass";
-import useSiteMetadata from "./SiteMetadata";
+// import "./all.sass";
+// import useSiteMetadata from "./SiteMetadata";
 import { withPrefix } from "gatsby";
 
 const TemplateWrapper = ({ children }) => {
-  const { title, description } = useSiteMetadata();
+  // const { title, description } = useSiteMetadata();
   return (
     <div>
       <Helmet>
         <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
-
+        {/* <title>{title}</title> */}
+        {/* <meta name="description" content={description} /> */}
+{/* 
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -37,11 +37,11 @@ const TemplateWrapper = ({ children }) => {
           rel="mask-icon"
           href={`${withPrefix("/")}img/safari-pinned-tab.svg`}
           color="#ff4400"
-        />
+        /> */}
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
+        {/* <meta property="og:title" content={title} /> */}
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      H5 Heading ! Проверка языка орков ыыыэъьцйфячсбю! Перевірка солов'їної.{" "}
+    </div>
+  );
+};
+export default HomePage;
