c9366f5d8c1c8c4dcb60c103c405b31d97516261
igorro79 <griniov.igor@gmail.com>
2022-11-11 00:18:44 +0200

    images to slick not importing

--- src/views/Hero.js
+++ src/views/Hero.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Grid } from "src/components";
 import { HeroTextBlock } from "src/features/HeroTextBlock/HeroTextBlock";
+import { Slick } from "components/Slider/Slider";
 
 export const Hero = () => {
   return (
     <section className="w-full" id="hero">
       <div className="container border-2">
+        <Slick />
         <Grid className="relative" section="hero">
           <HeroTextBlock />
           <div
