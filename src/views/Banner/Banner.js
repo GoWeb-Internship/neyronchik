f1681c0280265f455a00d8a21cf9675503657ab8
Ekaterina Aleksandrovych <ealeksandrovych@gmail.com>
2022-11-12 06:43:47 +0200

    fix styles

--- src/views/Banner/Banner.js
+++ src/views/Banner/Banner.js
@@ -1,29 +1,29 @@
 import React, {useState, useEffect} from 'react';
 // import * as s from './Banner.module.css'
-import './Banner.module.css'
+
 
 export const Banner = () => {
-  const [isVisible, setVisible] = useState(true)
+  const [isHidden, setHidden] = useState(false)
 
   // This runs when the page is loaded.
   useEffect(() => {
-    if (sessionStorage.getItem('visible-banner')) {
-        setVisible(false)
+    if (sessionStorage.getItem('hidden-banner')) {
+        setHidden(true)
     }
   }, [])
 
   const handleClick = () => {
-    sessionStorage.setItem('visible-banner', 'false')
-    setVisible(false)
+    sessionStorage.setItem('hidden-banner', 'true')
+    setHidden(true)
   }
 
   // const ClsBtn = <button onClick={handleClick}>X</button>
 
   return (
-     <div className={`${isVisible ? 'bannerIsShown' : 'bannerIsHidden'}`}>
-      <h2 >Акція місяця!!! </h2>
-      <strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong>
-      <button onClick={handleClick}>X</button>
+     <div className={`${isHidden ? 'hidden' : 'flex'}`}>
+      <h2 className='text-xl font-bold'>Акція місяця!!! </h2>
+      <strong className='ext-xl font-normal'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong>
+      <button className='h-[16px] w-[16px] flex justify-center' onClick={handleClick}>X</button>
     </div>
   )
 }
