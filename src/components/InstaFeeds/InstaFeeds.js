382ac509ac84d19dc52f83823e1f57c0103ba2fe
MishaDreamer-web <shondayn@gmail.com>
2022-11-15 01:52:51 +0200

    add instagram logic, fill .gitignore

---
+++ src/components/InstaFeeds/InstaFeeds.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+
+import Feed from "./Feed";
+
+import { fetchInstagramPosts } from "../../utils/instaFeedApi";
+
+const InstaFeeds = () => {
+  const [feeds, setFeedsData] = useState([]);
+  // console.log(feeds);
+
+  useEffect(() => {
+    const posts = fetchInstagramPosts();
+
+    posts.then((resp) => {
+      // console.log(resp === undefined);
+
+      if (resp === undefined) {
+        return;
+      }
+
+      setFeedsData(resp?.data.data);
+    });
+  }, []);
+
+  return (
+    <div className="container-feed">
+      {feeds && feeds.map((feed) => <Feed key={feed.id} feed={feed} />)}
+    </div>
+  );
+};
+
+export default InstaFeeds;
