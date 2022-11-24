08f12d6b5dea1019e05d3cc990d5d4492b31146d
galiovets <m.galiovets@gmail.com>
2022-11-24 17:59:27 +0200

    about img added

--- src/views/About.js
+++ src/views/About.js
@@ -7,7 +7,7 @@ import { HeroTextBlock } from "src/features/HeroTextBlock/HeroTextBlock";
 import { Headings } from "src/components/Headings/Headings";
 
 export const About = () => {
-  const { allMarkdownRemark } = useStaticQuery(
+  const { allMarkdownRemark, file } = useStaticQuery(
     graphql`
       query AboutQuery {
         allMarkdownRemark(
@@ -26,8 +26,10 @@ export const About = () => {
           }
         }
         file(name: { eq: "main" }) {
+          id
+          publicURL
           childImageSharp {
-            gatsbyImageData
+            gatsbyImageData(formats: [AUTO, WEBP])
           }
         }
       }
@@ -36,6 +38,7 @@ export const About = () => {
 
   const { language } = useI18next();
   const { edges } = allMarkdownRemark;
+  const aboutImg = file.childImageSharp.gatsbyImageData;
 
   return edges.map((edge) => {
     const { frontmatter } = edge.node;
@@ -47,7 +50,7 @@ export const About = () => {
           </Headings>
           <Grid className="relative" section="hero">
             <GatsbyImage
-              image={frontmatter.about_img}
+              image={aboutImg}
               alt={frontmatter[`${language}_about_title`]}
             />
             <p>{frontmatter[`${language}_about_body`]}</p>
@@ -56,39 +59,4 @@ export const About = () => {
       </section>
     );
   });
-
-  // return (
-  //   <section className="w-full " id="hero">
-  //     <div className="container border-2">
-  //       <Headings type="h2">Про нас</Headings>
-  //       <Grid className="relative" section="hero">
-  //         <div
-  //           id="image2"
-  //           style={{
-  //             width: "100%",
-  //             height: "300px",
-  //             backgroundColor: "yellow",
-  //           }}
-  //         ></div>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-  //           amet, viverra nec pretium fermentum ut cras. Id odio velit id
-  //           ultrices pretium aenean mauris neque, posuere. Varius nisi, ut fames
-  //           lorem scelerisque eget vitae. Porta proin rhoncus elementum, non at.
-  //           Eu aliquet etiam felis mi, eget justo, auctor diam nunc. Lectus non
-  //           ipsum dui urna, accumsan fermentum scelerisque interdum. Felis non
-  //           diam risus nibh dis at. Facilisis consequat libero, morbi erat
-  //           commodo eu congue. Rhoncus, elementum, vulputate adipiscing amet nam
-  //           vivamus mauris, lacinia. Nunc arcu in eget lorem lorem cras. Integer
-  //           aliquam purus turpis volutpat. Dolor id turpis porttitor morbi
-  //           ultrices fames aliquet. Malesuada blandit ornare volutpat dapibus.
-  //           Egestas eros augue pulvinar non fermentum in sit neque convallis.
-  //           Elementum amet integer in odio lobortis massa. Aliquam est, egestas
-  //           malesuada gravida. Egestas nisi risus nec duis odio dignissim. Amet
-  //           ultrices at bibendum fames id neq
-  //         </p>
-  //       </Grid>
-  //     </div>
-  //   </section>
-  // );
 };
