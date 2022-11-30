edd911ddb49eb1c915c8ad06f1ad621a3424b13b
Den <52295780+josserden@users.noreply.github.com>
2022-11-30 10:59:59 +0200

    upd config

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,17 @@
-import * as React from "react";
-import Footer from "components/Footer";
-import Navbar from "components/Navbar/Navbar";
+import * as React from 'react';
+import Footer from 'components/Footer';
+import Navbar from 'components/Navbar/Navbar';
 // import useSiteMetadata from "queries/SiteMetadata";
 // import { withPrefix } from "gatsby";
 
 const Layout = ({ children }) => {
   // const { title, description } = useSiteMetadata();
   return (
-    <div>
+    <>
       <Navbar />
-      <div>{children}</div>
+      <main>{children}</main>
       <Footer />
-    </div>
+    </>
   );
 };
 export default Layout;
