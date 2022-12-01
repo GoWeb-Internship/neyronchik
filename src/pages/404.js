e87fb00df9ddea0c7dd2ce5b5b5218b5b2524730
MishaDreamer-web <shondayn@gmail.com>
2022-12-01 17:59:05 +0200

    update page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,46 @@
 import * as React from "react";
-import Layout from "src/components/Layout";
+import { graphql } from "gatsby";
+// import Layout from "src/components/Layout";
+import { useI18next, useTranslation } from "gatsby-plugin-react-i18next";
 import { Link } from "gatsby";
 
-const NotFoundPage = () => (
-  <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <Link to="/">Go Home</Link>
-    </div>
-  </Layout>
-);
+const NotFoundPage = () => {
+  const { language } = useI18next();
+  const { t } = useTranslation();
+  const { title, description, button } = t("404", {
+    returnObjects: true,
+  });
+
+  return (
+    <main>
+      <section>
+        <div>
+          <h1>{title}</h1>
+          <p>{description}</p>
+          <Link
+            className="inline-block w-48 bg-green-500 pt-4  pb-4 text-center"
+            to="/"
+          >
+            {button}
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default NotFoundPage;
+
+export const pageQuery = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
