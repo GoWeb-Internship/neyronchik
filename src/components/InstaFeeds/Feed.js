e67535d582e929ebddfc3c0093aab1b5fb21557b
MishaDreamer-web <shondayn@gmail.com>
2022-12-07 19:34:28 +0200

    update news section

--- src/components/InstaFeeds/Feed.js
+++ src/components/InstaFeeds/Feed.js
@@ -4,7 +4,7 @@ import { BsArrowRight } from "react-icons/bs";
 const Feed = (props) => {
   const { id, caption, media_type, media_url, permalink } = props.feed;
   let post;
-  console.log(media_url);
+  // console.log(media_url);
 
   const sliceCaption = () => {
     if (caption && caption.length > 45) {
@@ -14,11 +14,11 @@ const Feed = (props) => {
       return caption;
     }
   };
-  console.log(sliceCaption().length);
+  // console.log(sliceCaption().length);
 
   const text =
     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis dicta at nulla!";
-  const sliceText = text.slice(0, 71).trim() + "...";
+  const sliceText = text.slice(0, 45).trim() + "...";
   // console.log(sliceText.length);
 
   switch (media_type) {
@@ -41,10 +41,8 @@ const Feed = (props) => {
       break;
     default:
       post = (
-        <div
-          className={` bg-insta-card shadow-insta-card mb-[20px] max-w-[360px] overflow-hidden rounded-[30px] border border-solid border-[#FFFFFF] p-3 backdrop-blur-sm`}
-        >
-          <div className="mb-3 flex">
+        <div className="min-w-[296px] max-w-[360px] overflow-hidden rounded-[30px] border border-solid border-[#FFFFFF] bg-insta-card p-3 shadow-insta-card backdrop-blur-sm md:p-5 notXl:mb-[20px]">
+          <div className="mb-3 flex md:mb-5">
             <div className="mr-3 h-[132px] w-[132px] shrink-0 overflow-hidden rounded-[20px]">
               <img
                 className="h-[inherit] object-cover"
@@ -55,21 +53,28 @@ const Feed = (props) => {
                 alt={caption}
               />
             </div>
-            <div className="flex max-h-[132px] items-center pt-3 pb-3">
-              <p className="text-insta-black text-sm leading-[150%]">
-                {sliceCaption()}
+            <div className="flex max-h-[132px] items-center pb-3 pt-3 smOnly:min-w-[128px] md:w-44">
+              <p className="text-sm font-medium leading-[150%] text-insta-black mdOnly:text-base">
+                {sliceCaption() || sliceText}
               </p>
             </div>
           </div>
           <a
-            className="text-insta-white shadow-insta-button flex h-11 w-full items-center justify-center rounded-[30px] border border-solid border-[#E11D48] bg-[#E11D48] p-[11px] text-center"
+            className="flex h-11 w-full items-center justify-center rounded-[30px] border border-solid border-[#E11D48] bg-[#E11D48] p-[11px] text-center text-insta-white shadow-insta-button sm:h-12 mdOnly:text-base"
             href={permalink}
             rel="nofollow noreferrer noopener"
             target="_blank"
             aria-label="instagram"
           >
             Дізнатись більше
-            <BsArrowRight size={21} className="text-insta-white ml-[6px]" />
+            <BsArrowRight
+              size={21}
+              className="ml-[6px] text-insta-white md:hidden"
+            />
+            <BsArrowRight
+              size={28}
+              className="ml-[6px] hidden text-insta-white md:block"
+            />
           </a>
         </div>
       );
--- src/views/News/News.js
+++ src/views/News/News.js
@@ -6,7 +6,7 @@ import { Headings } from "src/components/Headings/Headings";
 
 import InstaFeeds from "../../components/InstaFeeds/InstaFeeds";
 
-import { newsBg } from "./News.module.css";
+import { newsBg, textShadow } from "./News.module.css";
 
 export const News = () => {
   const { t } = useTranslation();
@@ -15,9 +15,11 @@ export const News = () => {
   });
 
   return (
-    <section className={`w-full pb-14 ${newsBg}`} id="news">
-      <div className="pr-3 pl-3">
-        <h1 className="mb-10 text-center font-adigiana text-[40px] font-normal leading-[100%] text-[#0EA5E9]">
+    <section className={`w-full ${newsBg} pb-14`} id="news">
+      <div className="md:mr-auto md:ml-auto md:w-[768px] xl:w-[1116px] notXl:pr-3 notXl:pl-3">
+        <h1
+          className={`${textShadow} mb-10 text-center font-adigiana text-[40px] font-normal leading-[100%] text-[#0EA5E9] md:text-[52px] md:leading-[150%]`}
+        >
           {news_title}
         </h1>
 
