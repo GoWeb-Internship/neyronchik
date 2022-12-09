1e4f7acd8c9ce8c8d8baf6421cb55665d37bf85d
galiovets <m.galiovets@gmail.com>
2022-12-09 12:53:20 +0200

    added translations to locales

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,14 +4,18 @@ import * as s from "./Footer.module.css";
 
 const Footer = () => {
   const { t } = useTranslation();
-  const { neyronchiki, rights } = t("footer", {
+  // const { neyronchiki, rights } = t("footer", {
+  //   returnObjects: true,
+  // });
+  const { title, weAre, rights } = t("page", {
     returnObjects: true,
   });
 
   return (
     <footer className="w-full text-neutral-900" id="footer">
       <div className={s.wrapper}>
-        &#169; {new Date().getFullYear()} {neyronchiki} {""} {rights}
+        &#169; {new Date().getFullYear()} {title} - {weAre.toLowerCase()}.{" "}
+        {rights}
       </div>
     </footer>
   );
